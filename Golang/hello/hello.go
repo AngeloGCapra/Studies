81b2538ad0e24e5f49be9d5fe528b3e57beb633c
AngeloGCapra <angelo.gabriel.capra@gmail.com>
2026-04-21 19:27:17 -0300

    Basics of Go

--- Golang/hello/hello.go
+++ Golang/hello/hello.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 import "rsc.io/quote"
 
 func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println(quote.Go())
+	fmt.Println("The time is", time.Now())
 }
---
+++ Golang/tour/basics/imports.go
@@ -0,0 +1,20 @@
+package main
+
+/*
+This code groups the imports into a parenthesized, "factored" import statement.
+You can also write multiple import statements, like:
+
+import "fmt"
+import "math"
+
+But it is good style to use the factored import statement.
+*/
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+}
